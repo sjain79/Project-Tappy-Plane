fad15e50ecb32f83454c2d6282dc02600710cd71
Juan <juandiegoalzatemejia@gmail.com>
2018-05-17 19:07:49 -0400

    Added ScrollingScript.cs

---
+++ Project Tappy Plane/Assets/ScrollingObjectScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+    [SerializeField] Rigidbody2D rb;
+    public float scrollSpeed = 1.5f;
+
+
+	// Use this for initialization
+	void Start () {
+        rb.velocity = Vector3.left * scrollSpeed;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(PlaneScript.isPlayerDead)
+        {
+            rb.velocity = Vector3.zero;
+        }
+	}
+}
