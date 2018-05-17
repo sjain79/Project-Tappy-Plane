d95c9cc8cf2999354331b86283c22bd16df16b91
Juan <juandiegoalzatemejia@gmail.com>
2018-05-17 15:55:08 -0400

    Added Fly Functionality
    Plane will go up if the space bar is pressed

---
+++ Project Tappy Plane/Assets/PlaneScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaneScript : MonoBehaviour {
+
+    [SerializeField]
+    float verticalSpeed = 0.5f;
+    [SerializeField]
+    Rigidbody2D rb;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.velocity = Vector3.up * verticalSpeed;
+        }
+		
+	}
+}
