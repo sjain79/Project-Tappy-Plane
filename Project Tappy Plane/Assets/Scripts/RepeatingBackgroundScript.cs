f8b7ea9125287df1cd67c36385c133c73e95cfea
Juan <juandiegoalzatemejia@gmail.com>
2018-05-18 14:51:50 -0400

    Created Repeating Background Script

---
+++ Project Tappy Plane/Assets/Scripts/RepeatingBackgroundScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RepeatingBackgroundScript : MonoBehaviour {
+    PolygonCollider2D groundCollider;
+    float groundColliderLength;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
