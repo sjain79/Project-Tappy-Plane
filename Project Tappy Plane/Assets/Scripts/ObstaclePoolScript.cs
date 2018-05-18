ab64a9474d9002b381e50efa63462449df53bbdd
Juan <juandiegoalzatemejia@gmail.com>
2018-05-18 19:20:50 -0400

    Added Obstacles and began Obstacle Pooling

---
+++ Project Tappy Plane/Assets/Scripts/ObstaclePoolScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstaclePoolScript : MonoBehaviour {
+    [SerializeField] int obstaclePoolSize = 5;
+    GameObject[] obstacles;
+    [SerializeField] GameObject obstaclePrefab;
+	// Use this for initialization
+	void Start () {
+        obstacles = new GameObject[obstaclePoolSize];
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
