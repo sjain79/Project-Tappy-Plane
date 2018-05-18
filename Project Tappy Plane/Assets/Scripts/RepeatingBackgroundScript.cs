65095d0aaac80c54371de100f1dfeae9176bd284
Juan <juandiegoalzatemejia@gmail.com>
2018-05-18 15:00:28 -0400

    Added Infinite Level Functionality
    -Background and ground position themselves in front of the camera after they go out of camera bounds

--- Project Tappy Plane/Assets/Scripts/RepeatingBackgroundScript.cs
+++ Project Tappy Plane/Assets/Scripts/RepeatingBackgroundScript.cs
@@ -8,11 +8,19 @@ public class RepeatingBackgroundScript : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		
+        groundCollider = gameObject.GetComponent<PolygonCollider2D>();
+        groundColliderLength = groundCollider.bounds.extents.x * 2f;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        if (transform.position.x < -groundColliderLength)
+            RepositionBackground();
 	}
+
+    void RepositionBackground()
+    {
+        Vector2 offset = new Vector2(groundColliderLength * 2f, 0);
+        transform.position = (Vector2)transform.position + offset;
+    }
 }
