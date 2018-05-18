35e903dfec03931da21a5dd9428fd8ca8aeda9fd
Unknown <sjain@student.fullsail.edu>
2018-05-18 04:52:03 -0400

    Started UI implementation
    Tap to play UI implemented
    Score system implemented  (no font was available)

---
+++ Project Tappy Plane/Assets/InGameUIScript.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InGameUIScript : MonoBehaviour
+{
+    [SerializeField]
+    Image scoreUnitsPlace, scoreTensPlace, scoreHundredsPlace;
+
+    bool scoreCrossed100 = false;
+
+    [SerializeField]
+    Sprite[] numbers;
+
+    private void Start()
+    {
+        scoreUnitsPlace.enabled = false;
+        scoreTensPlace.enabled = false;
+        scoreHundredsPlace.enabled = false;
+    }
+
+    private void Update()
+    {
+        ShowScore();
+    }
+
+    private void ShowScore()
+    {
+        if (!scoreCrossed100)
+        {
+            if (PlaneScript.score < 10)
+            {
+                scoreUnitsPlace.enabled = true;
+                scoreUnitsPlace.transform.localPosition = new Vector2(0, 0);
+            }
+            else if (PlaneScript.score >= 10 && PlaneScript.score < 100)
+            {
+                scoreTensPlace.enabled = true;
+
+                scoreUnitsPlace.transform.localPosition = new Vector2(26.5f, 0);
+                scoreTensPlace.transform.position = new Vector2(-26.5f, 0);
+            }
+            else if (PlaneScript.score >= 100)
+            {
+                scoreHundredsPlace.enabled = true;
+
+                scoreUnitsPlace.transform.localPosition = new Vector2(53, 0);
+                scoreTensPlace.transform.position = new Vector2(0, 0);
+                scoreHundredsPlace.transform.position = new Vector2(-53f, 0);
+
+                scoreCrossed100 = true;
+            }
+        }
+
+        scoreUnitsPlace.sprite = numbers[PlaneScript.score % 10];
+        scoreTensPlace.sprite = numbers[(PlaneScript.score/10) % 10];
+        scoreHundredsPlace.sprite = numbers[(PlaneScript.score/100) % 10];
+    }
+}
---
+++ Project Tappy Plane/Assets/MenuScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuScript : MonoBehaviour {
+
+    [SerializeField]
+    RectTransform tapParent;
+
+    GameObject plane;
+
+    private void Start()
+    {
+        plane = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    private void Update()
+    {
+        SetTapImage();
+    }
+
+
+    private void SetTapImage()
+    {
+        if (GameController.gameState == GameState.Menu)
+            tapParent.transform.position = plane.transform.position;
+    }
+}
