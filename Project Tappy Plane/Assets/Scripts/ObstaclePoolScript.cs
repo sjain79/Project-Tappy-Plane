4123103bfb77503f2808878d52ef41b57e2fac09
Juan <juandiegoalzatemejia@gmail.com>
2018-05-19 18:15:26 -0400

    Working on Animations and Obstacles

--- Project Tappy Plane/Assets/Scripts/ObstaclePoolScript.cs
+++ Project Tappy Plane/Assets/Scripts/ObstaclePoolScript.cs
@@ -3,13 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ObstaclePoolScript : MonoBehaviour {
-    [SerializeField] int obstaclePoolSize = 5;
+    [SerializeField] int obstaclePoolSize = 10;
     GameObject[] obstacles;
-    public GameObject obstaclePrefab;
+    public GameObject obstaclePrefab1, obstaclePrefab2, obstaclePrefab3, obstaclePrefab4;
     float timeSinceLastSpawned = 0;
     [SerializeField] float spawnPositionX = 10f;
     int currentObstacle = 0;
-    [SerializeField] float spawnFrequency = 2f;
+    [SerializeField] float spawnFrequency = 4f;
     [SerializeField] float spawnPositionY = 0f;
 
 	// Use this for initialization
@@ -17,7 +17,15 @@ public class ObstaclePoolScript : MonoBehaviour {
         obstacles = new GameObject[obstaclePoolSize];
         for (int i = 0; i < obstaclePoolSize; i++)
         {
-            obstacles[i] = (GameObject)Instantiate(obstaclePrefab, new Vector2(-15f, -25f), Quaternion.identity);
+            if(i%4 == 1)
+            obstacles[i] = (GameObject)Instantiate(obstaclePrefab1, new Vector2(-15f, -25f), Quaternion.identity);
+            else if (i % 4 == 2)
+                obstacles[i] = (GameObject)Instantiate(obstaclePrefab2, new Vector2(-15f, -25f), Quaternion.identity);
+            else if (i % 4 == 3)
+                obstacles[i] = (GameObject)Instantiate(obstaclePrefab3, new Vector2(-15f, -25f), Quaternion.identity);
+            else if (i % 4 == 0)
+                obstacles[i] = (GameObject)Instantiate(obstaclePrefab4, new Vector2(-15f, -25f), Quaternion.identity);
+
         }
 	}
 	
@@ -31,11 +39,17 @@ public class ObstaclePoolScript : MonoBehaviour {
             if (timeSinceLastSpawned >= spawnFrequency)
             {
                 timeSinceLastSpawned = 0;
-                obstacles[currentObstacle].transform.position = new Vector2(spawnPositionX, spawnPositionY);
+                if(currentObstacle % 4 == 0)
+                {
+                    spawnPositionY = Random.Range(-0.3f, 3.06f);
+                    obstacles[currentObstacle].transform.position = new Vector2(spawnPositionX, spawnPositionY);
+                    spawnPositionY = 0f;
+                }
+                else
+                    obstacles[currentObstacle].transform.position = new Vector2(spawnPositionX, spawnPositionY);
                 currentObstacle++;
                 if (currentObstacle >= obstaclePoolSize)
                     currentObstacle = 0;
-                Debug.Log(obstacles[currentObstacle].transform.position.x);
             }
         }
 	}
