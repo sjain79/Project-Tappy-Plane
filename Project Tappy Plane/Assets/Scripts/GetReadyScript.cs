2fb0bafc471db9d9843bd2d370b7101435bf31fb
Unknown <sjain@student.fullsail.edu>
2018-05-19 17:23:05 -0400

    Made Get Ready Animation

---
+++ Project Tappy Plane/Assets/Scripts/GetReadyScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GetReadyScript : MonoBehaviour {
+
+    [SerializeField]
+    GameObject inGamePanel,startGamePanel;
+
+    public void StartGame()
+    {
+        Invoke("DelayedStartGame", 0.5f);
+    }
+
+    private void DelayedStartGame()
+    {
+        inGamePanel = transform.parent.gameObject.transform.GetChild(1).gameObject;
+        inGamePanel.SetActive(true);
+        startGamePanel.SetActive(false);
+        GameController.gameState = GameState.Playing;
+    }
+}
