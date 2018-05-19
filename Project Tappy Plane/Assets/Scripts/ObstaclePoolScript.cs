d6b2ad25cd27d2dcabc0929c16cd259a5ddf55d6
Juan <juandiegoalzatemejia@gmail.com>
2018-05-18 20:39:41 -0400

    ObstaclePoolScript functionality attempt

--- Project Tappy Plane/Assets/Scripts/ObstaclePoolScript.cs
+++ Project Tappy Plane/Assets/Scripts/ObstaclePoolScript.cs
@@ -6,13 +6,36 @@ public class ObstaclePoolScript : MonoBehaviour {
     [SerializeField] int obstaclePoolSize = 5;
     GameObject[] obstacles;
     [SerializeField] GameObject obstaclePrefab;
+    float timeSinceLastSpawned = 0;
+    [SerializeField] float spawnPositionX = 30f;
+    int currentObstacle = 0;
+    [SerializeField] float spawnFrequency = 4f;
+    [SerializeField] float spawnPositionY = 0f;
+
 	// Use this for initialization
 	void Start () {
         obstacles = new GameObject[obstaclePoolSize];
+        for (int i = 0; i < obstaclePoolSize; i++)
+        {
+            obstacles[i] = (GameObject)Instantiate(obstaclePrefab, new Vector2(-15f, -25f), Quaternion.identity);
+        }
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void Update ()
+    {
+        if (GameController.gameState == GameState.Playing)
+        {
+            timeSinceLastSpawned += Time.deltaTime;
+
+            if (timeSinceLastSpawned >= spawnFrequency)
+            {
+                obstacles[currentObstacle].transform.position = new Vector2(spawnPositionX, spawnPositionY);
+                currentObstacle++;
+                if (currentObstacle >= obstaclePoolSize)
+                    currentObstacle = 0;
+                timeSinceLastSpawned = 0;
+            }
+        }
 	}
 }
