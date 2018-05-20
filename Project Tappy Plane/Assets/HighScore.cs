8d630e7fa2e702b40a09593bb0c0c8a2ed77c462
Unknown <sjain@student.fullsail.edu>
2018-05-19 21:36:28 -0400

    HighScore and GameOver screen implemented

---
+++ Project Tappy Plane/Assets/HighScore.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HighScore : MonoBehaviour
+{
+    [SerializeField]
+    GameObject newHighScoreText, showHighScoreText, highScoreNumber;
+
+    private void Start()
+    {
+        if (PlayerPrefs.GetInt("Highscore",0)<PlaneScript.score)
+        {
+            newHighScoreText.SetActive(true);
+            highScoreNumber.SetActive(true);
+            PlayerPrefs.SetInt("Highscore", PlaneScript.score);
+        }
+        else
+        {
+            showHighScoreText.SetActive(true);
+        }
+    }
+}
--- Project Tappy Plane/Assets/Scripts/GameOverPanelScript.cs
+++ Project Tappy Plane/Assets/Scripts/GameOverPanelScript.cs
@@ -4,12 +4,15 @@ using UnityEngine;
 
 public class GameOverPanelScript : MonoBehaviour
 {
+    [SerializeField]
+    GameObject gameOverPanel, gameOverText;
 
     private void Update()
     {
         if (GameController.gameState == GameState.Gameover)
         {
-
+            gameOverPanel.SetActive(true);
+            gameOverText.SetActive(true);
         }
     }
 }
