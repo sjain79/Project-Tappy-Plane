62d5e4e000d5ecbefcb599f05769b7d20084a1e3
Unknown <sjain@student.fullsail.edu>
2018-05-24 00:42:38 -0300

    Removed the mini profiler and also fixed the star problem

---
+++ Project Tappy Plane/Assets/Scripts/MoveStar.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveStar : MonoBehaviour {
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        transform.position =  transform.position + new Vector3(0.2f, 0);
+    }
+}
